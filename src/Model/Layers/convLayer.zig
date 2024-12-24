be85fb2ec4895b0f41326fb789d83d0e4300b8aa
MirkoCalvi <mirkocalvi.job@gmail.com>
2024-12-24 16:55:29 +0100

    conv layer forward working, max pooling has bugs, conv backward WIP

--- src/Model/Layers/convLayer.zig
+++ src/Model/Layers/convLayer.zig
@@ -8,14 +8,14 @@ const LayerError = @import("errorHandler").LayerError;
 pub fn ConvolutionalLayer(comptime T: type) type {
     return struct {
         // Convolutional layer parameters
-        weights: Tensor.Tensor(T), // Weights (kernels) of shape [out_channels, in_channels, kernel_height, kernel_width]
+        weights: Tensor.Tensor(T), // Weights (kernels) of shape [kernel_shape]
         bias: Tensor.Tensor(T), // Biases for each output channel
         input: Tensor.Tensor(T), // Input tensor
         output: Tensor.Tensor(T), // Output tensor after convolution
         // Layer configuration
         input_channels: usize,
-        output_channels: usize,
-        kernel_size: [2]usize, // [kernel_height, kernel_width]
+        kernel_shape: [4]usize, // shape : [number of kernel filters, number of channels, height, width ]
+        stride: [2]usize,
         // Gradients
         w_gradients: Tensor.Tensor(T),
         b_gradients: Tensor.Tensor(T),
@@ -45,35 +45,51 @@ pub fn ConvolutionalLayer(comptime T: type) type {
         /// Initialize the convolutional layer with random weights and biases
         pub fn init(ctx: *anyopaque, alloc: *const std.mem.Allocator, args: *anyopaque) !void {
             const self: *Self = @ptrCast(@alignCast(ctx));
-            const argsStruct: *const struct { input_channels: usize, output_channels: usize, kernel_size: [2]usize } = @ptrCast(@alignCast(args));
-            const input_channels = argsStruct.input_channels;
-            const output_channels = argsStruct.output_channels;
-            const kernel_size = argsStruct.kernel_size;
 
-            // Check parameters
-            if (input_channels <= 0 or output_channels <= 0) return LayerError.InvalidParameters;
-            if (kernel_size[0] <= 0 or kernel_size[1] <= 0) return LayerError.InvalidParameters;
+            const argsStruct: *const struct {
+                input_channels: usize,
+                kernel_shape: [4]usize,
+                stride: [2]usize,
+            } = @ptrCast(@alignCast(args));
 
             // Initialize layer configuration
-            self.input_channels = input_channels;
-            self.output_channels = output_channels;
-            self.kernel_size = kernel_size;
+            self.input_channels = argsStruct.input_channels;
+            self.kernel_shape = argsStruct.kernel_shape;
             self.allocator = alloc;
+            self.stride = argsStruct.stride;
+
+            //assuming kernel is always of size 4
+            if (self.kernel_shape.len != 4) {
+                std.debug.print("\nERROR : kernel must always be of size 4, while yours is {}, kernel_shape:{any}\nThe values have the following meaning:", .{ self.kernel_shape.len, self.kernel_shape });
+                std.debug.print("\n     kernel_shape[0] = number of kernel filters", .{});
+                std.debug.print("\n     kernel_shape[1] = number of channels for each filter", .{});
+                std.debug.print("\n     kernel_shape[2] = height of each channel", .{});
+                std.debug.print("\n     kernel_shape[3] = width of each channel", .{});
+                return LayerError.InvalidParameters;
+            }
+
+            // Check parameters
+            if (self.input_channels <= 0) return LayerError.InvalidParameters;
+            if (self.kernel_shape[0] <= 0 or self.kernel_shape[1] <= 0 or self.kernel_shape[2] <= 0 or self.kernel_shape[3] <= 0) return LayerError.InvalidParameters;
+
+            //check channels
+            if (self.input_channels != self.kernel_shape[1]) {
+                std.debug.print("\nERROR : Input and K ernel must have the same number of channels", .{});
+            }
 
             // Initialize weights and biases
-            var weight_shape: [4]usize = [_]usize{ output_channels, input_channels, kernel_size[0], kernel_size[1] };
-            var bias_shape: [2]usize = [_]usize{ output_channels, 1 };
+            var bias_shape: [1]usize = [_]usize{self.kernel_shape[0]}; //one bias for each filter
 
-            const weight_array = try Layer.randn(T, self.allocator, 1, output_channels * input_channels * kernel_size[0] * kernel_size[1]);
+            const weight_array = try Layer.randn(T, self.allocator, 1, self.kernel_shape[0] * self.kernel_shape[1] * self.kernel_shape[2] * self.kernel_shape[3]);
             defer self.allocator.free(weight_array);
-            const bias_array = try Layer.randn(T, self.allocator, 1, output_channels);
+            const bias_array = try Layer.randn(T, self.allocator, 1, self.kernel_shape[0]);
             defer self.allocator.free(bias_array);
 
-            self.weights = try Tensor.Tensor(T).fromArray(alloc, weight_array, &weight_shape);
+            self.weights = try Tensor.Tensor(T).fromArray(alloc, weight_array, &self.kernel_shape);
             self.bias = try Tensor.Tensor(T).fromArray(alloc, bias_array, &bias_shape);
 
-            // Initialize gradients
-            self.w_gradients = try Tensor.Tensor(T).fromShape(self.allocator, &weight_shape);
+            // Initialize gradients to zero
+            self.w_gradients = try Tensor.Tensor(T).fromShape(self.allocator, &self.kernel_shape);
             self.b_gradients = try Tensor.Tensor(T).fromShape(self.allocator, &bias_shape);
         }
 
@@ -120,7 +136,7 @@ pub fn ConvolutionalLayer(comptime T: type) type {
             self.input = try input.copy();
 
             // Perform convolution operation
-            self.output = try TensMath.convolve_tensor_with_bias(T, T, &self.input, &self.weights, &self.bias);
+            self.output = try TensMath.convolve_tensor_with_bias(T, T, &self.input, &self.weights, &self.bias, &self.stride);
             //self.output.info();
 
             return self.output;
@@ -138,26 +154,29 @@ pub fn ConvolutionalLayer(comptime T: type) type {
                 self.b_gradients.deinit();
             }
 
+            _ = dValues;
+
             // Compute gradients with respect to biases
             // Sum over the spatial dimensions
-            self.b_gradients = TensMath.convolution_backward_biases(T, dValues) catch |err| {
-                std.debug.print("Error during conv backward_biases {any}", .{err});
-                return err;
-            };
+            // self.b_gradients = TensMath.convolution_backward_biases(T, dValues) catch |err| {
+            //     std.debug.print("Error during conv backward_biases {any}", .{err});
+            //     return err;
+            // };
 
-            // Compute gradients with respect to weights
-            self.w_gradients = TensMath.convolution_backward_weights(T, &self.input, dValues) catch |err| {
-                std.debug.print("Error during conv backward_weights {any}", .{err});
-                return err;
-            };
+            // // Compute gradients with respect to weights
+            // self.w_gradients = TensMath.convolution_backward_weights(T, &self.input, dValues) catch |err| {
+            //     std.debug.print("Error during conv backward_weights {any}", .{err});
+            //     return err;
+            // };
 
-            // Compute gradients with respect to input
-            var dInput = TensMath.convolution_backward_input(T, dValues, &self.weights) catch |err| {
-                std.debug.print("Error during conv backward_input {any}", .{err});
-                return err;
-            };
-            _ = &dInput;
-            return dInput;
+            // // Compute gradients with respect to input
+            // var dInput = TensMath.convolution_backward_input(T, dValues, &self.weights) catch |err| {
+            //     std.debug.print("Error during conv backward_input {any}", .{err});
+            //     return err;
+            // };
+            // _ = &dInput;
+
+            return self.w_gradients; //TODO: CHANGEEEEE!!! JUST FOR DEBUG
         }
 
         /// Print the convolutional layer information (To be written)
@@ -180,7 +199,7 @@ pub fn ConvolutionalLayer(comptime T: type) type {
             const self: *Self = @ptrCast(@alignCast(ctx));
 
             // For convolutional layers, n_neurons can be considered as output_channels
-            return self.output_channels;
+            return self.kernel_shape[1];
         }
 
         pub fn get_weights(ctx: *anyopaque) *const Tensor.Tensor(T) {
