ca9b8399d20cd70c8253cc217f0afc315700835b
MirkoCalvi <mirkocalvi.job@gmail.com>
2025-01-13 11:17:19 +0100

    most of Tensor method mooved in TensorMath

--- src/Core/Tensor/TensorMath/basic_math.zig
+++ src/Core/Tensor/TensorMath/basic_math.zig
@@ -52,41 +52,6 @@ fn add_bias_thread(comptime T: anytype, array: []T, start: usize, len: usize, bi
         array[start + i] += bias.data[i];
     }
 }
-/// Performs the mean of a given tensor. It is a reduction operation, collapsing the whole tenosr into a single value.
-pub fn mean(comptime T: anytype, tensor: *Tensor(T)) f32 {
-    var res: f32 = 0;
-
-    for (tensor.data) |*d| {
-        res += Converter.convert(T, f32, d.*);
-    }
-    res = res / Converter.convert(usize, f32, tensor.size);
-    return res;
-}
-
-pub fn equal(comptime T: anytype, t1: *Tensor(T), t2: *Tensor(T)) bool {
-    //same size
-    if (t1.size != t2.size) {
-        std.debug.print("\n\n ERROR:WRONG SIZE t1.size:{} t2.size:{}", .{ t1.size, t2.size });
-        return false;
-    }
-
-    //same shape
-    for (0..t1.shape.len) |i| {
-        if (t1.shape[i] != t2.shape[i]) {
-            std.debug.print("\n\n ERROR: WRONG SHAPE t1.shape[{}]:{} t2.shape[{}]:{}", .{ i, t1.shape[i], i, t2.shape[i] });
-            return false;
-        }
-    }
-
-    //same data
-    if (!std.mem.eql(T, t1.data, t2.data)) {
-        std.debug.print("\n\n ERROR: WRONG DATA", .{});
-        return false;
-    }
-
-    return true;
-}
-
 ///Returns a Tensor with the same shape pf t1 and t2, where each element --> out[location] = t1[location] + t2[location]
 pub fn sum_tensors(comptime arch: Architectures, comptime Tin: anytype, comptime Tout: anytype, t1: *Tensor(Tin), t2: *Tensor(Tin)) !Tensor(Tout) {
 
@@ -290,4 +255,99 @@ fn CPU_sub_tensors(comptime inputType: anytype, comptime outputType: anytype, t1
         .size = total_size,
         .allocator = &pkg_allocator,
     };
+}
+
+pub fn mul(comptime T: anytype, lhs: *Tensor(T), rhs: *Tensor(T)) !Tensor(T) {
+    if (lhs.size != rhs.size) {
+        return TensorError.MismatchedShape;
+    }
+
+    const allocator = lhs.allocator;
+    const result = try Tensor(T).fromShape(allocator, lhs.shape);
+
+    for (0..lhs.size) |i| {
+        result.data[i] = lhs.data[i] * rhs.data[i];
+    }
+
+    return result;
+}
+
+/// Performs the mean of a given tensor. It is a reduction operation, collapsing the whole tenosr into a single value.
+pub fn mean(comptime T: anytype, tensor: *Tensor(T)) f32 {
+    var res: f32 = 0;
+
+    for (tensor.data) |*d| {
+        res += Converter.convert(T, f32, d.*);
+    }
+    res = res / Converter.convert(usize, f32, tensor.size);
+    return res;
+}
+
+pub fn equal(comptime T: anytype, t1: *Tensor(T), t2: *Tensor(T)) bool {
+    //same size
+    if (t1.size != t2.size) {
+        std.debug.print("\n\n ERROR:WRONG SIZE t1.size:{} t2.size:{}", .{ t1.size, t2.size });
+        return false;
+    }
+
+    //same shape
+    for (0..t1.shape.len) |i| {
+        if (t1.shape[i] != t2.shape[i]) {
+            std.debug.print("\n\n ERROR: WRONG SHAPE t1.shape[{}]:{} t2.shape[{}]:{}", .{ i, t1.shape[i], i, t2.shape[i] });
+            return false;
+        }
+    }
+
+    //same data
+    if (!std.mem.eql(T, t1.data, t2.data)) {
+        std.debug.print("\n\n ERROR: WRONG DATA", .{});
+        return false;
+    }
+
+    return true;
+}
+
+/// Returns true if the Tensor is one-hot encoded
+fn isOneHot(comptime T: anytype, t: *Tensor(T)) !bool {
+    const elems_row = t.shape[t.shape.len - 1];
+    if (elems_row == 0) {
+        return TensorError.EmptyTensor;
+    }
+    const numb_rows = t.size / elems_row;
+    if (numb_rows == 0) {
+        return TensorError.ZeroSizeTensor;
+    }
+
+    for (0..numb_rows) |row| {
+        var oneHotFound = false;
+        for (0..t.shape[t.shape.len - 1]) |i| {
+            if (t.data[row * elems_row + i] == 1 and !oneHotFound) {
+                if (!oneHotFound) oneHotFound = true else return TensorError.NotOneHotEncoded;
+            }
+        }
+    }
+
+    return true;
+}
+
+/// Returns true only if all the values of shape and data are valid numbers
+pub fn isSafe(comptime T: anytype, t: *Tensor(T)) !void {
+    switch (@typeInfo(T)) {
+        .Float => {
+            // Loop over tensor data
+            for (t.data) |*value| {
+                if (std.math.isNan(value.*)) return TensorError.NanValue;
+                if (!std.math.isFinite(value.*)) return TensorError.NotFiniteValue;
+            }
+
+            // Loop over tensor shape
+            for (t.shape) |*value| {
+                if (std.math.isNan(value.*)) return TensorError.NanValue;
+            }
+        },
+        else => {
+            // If T is not Float, skip isSafe checks
+            return;
+        },
+    }
 }
