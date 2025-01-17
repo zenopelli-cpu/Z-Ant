f0394c70d6fb973a6183862ed6928aeaf00d8cc9
MirkoCalvi <mirkocalvi.job@gmail.com>
2025-01-17 15:00:50 +0100

    removed all mathematical modules from build.zig

--- src/Core/Tensor/TensorMath/tensor_math.zig
+++ src/Core/Tensor/TensorMath/tensor_math.zig
@@ -3,39 +3,44 @@
 // ---------------------------------------------------------------------------
 //
 // ---------- importing standard basic methods ----------
-pub const add_bias = @import("basic_math").add_bias;
-pub const sum_tensors = @import("basic_math").sum_tensors;
-pub const sub_tensors = @import("basic_math").sub_tensors;
-pub const mul = @import("basic_math").mul;
-pub const isOneHot = @import("basic_math").isOneHot;
-pub const isSafe = @import("basic_math").isSafe;
+const basic_math_lib = @import("basic_math.zig");
+pub const add_bias = basic_math_lib.add_bias;
+pub const sum_tensors = basic_math_lib.sum_tensors;
+pub const sub_tensors = basic_math_lib.sub_tensors;
+pub const mul = basic_math_lib.mul;
+pub const isOneHot = basic_math_lib.isOneHot;
+pub const isSafe = basic_math_lib.isSafe;
 
 // ---------- importing standard reduction and logical methods ----------
-pub const equal = @import("basic_math").equal;
-pub const mean = @import("basic_math").mean;
+pub const equal = basic_math_lib.equal;
+pub const mean = basic_math_lib.mean;
 
 // ---------- importing standard strucutal methods ----------
-pub const concatenate = @import("structural_math").concatenate;
-pub const calculateStrides = @import("structural_math").calculateStrides;
-pub const transpose2D = @import("structural_math").transpose2D;
-pub const transposeDefault = @import("structural_math").transposeDefault;
-pub const addPaddingAndDilation = @import("structural_math").addPaddingAndDilation;
-pub const flip = @import("structural_math").flip;
-pub const resize = @import("structural_math").resize;
+const structural_math_lib = @import("structural_math.zig");
+pub const concatenate = structural_math_lib.concatenate;
+pub const calculateStrides = structural_math_lib.calculateStrides;
+pub const transpose2D = structural_math_lib.transpose2D;
+pub const transposeDefault = structural_math_lib.transposeDefault;
+pub const addPaddingAndDilation = structural_math_lib.addPaddingAndDilation;
+pub const flip = structural_math_lib.flip;
+pub const resize = structural_math_lib.resize;
 
 // ---------- importing matrix algebra methods ----------
-pub const compute_dot_product = @import("algebraic_math").compute_dot_product;
-pub const dot_product_tensor = @import("algebraic_math").dot_product_tensor;
+const algebraic_math_lib = @import("algebraic_math.zig");
+pub const compute_dot_product = algebraic_math_lib.compute_dot_product;
+pub const dot_product_tensor = algebraic_math_lib.dot_product_tensor;
 
 // ---------- importing standard convolution methods ----------
-pub const multidim_convolution_with_bias = @import("convolution_math").multidim_convolution_with_bias;
-pub const convolve_tensor_with_bias = @import("convolution_math").convolve_tensor_with_bias;
-pub const convolution_backward_biases = @import("convolution_math").convolution_backward_biases;
-pub const convolution_backward_weights = @import("convolution_math").convolution_backward_weights;
-pub const convolution_backward_input = @import("convolution_math").convolution_backward_input;
+const convolution_math_lib = @import("convolution_math.zig");
+pub const multidim_convolution_with_bias = convolution_math_lib.multidim_convolution_with_bias;
+pub const convolve_tensor_with_bias = convolution_math_lib.convolve_tensor_with_bias;
+pub const convolution_backward_biases = convolution_math_lib.convolution_backward_biases;
+pub const convolution_backward_weights = convolution_math_lib.convolution_backward_weights;
+pub const convolution_backward_input = convolution_math_lib.convolution_backward_input;
 
 // ---------- importing standard pooling methods ----------
-pub const pool_tensor = @import("pooling_math").pool_tensor;
-pub const multidim_pooling = @import("pooling_math").multidim_pooling;
-pub const pool_forward = @import("pooling_math").pool_forward;
-pub const pool_backward = @import("pooling_math").pool_backward;
+const pooling_math_lib = @import("pooling_math.zig");
+pub const pool_tensor = pooling_math_lib.pool_tensor;
+pub const multidim_pooling = pooling_math_lib.multidim_pooling;
+pub const pool_forward = pooling_math_lib.pool_forward;
+pub const pool_backward = pooling_math_lib.pool_backward;
