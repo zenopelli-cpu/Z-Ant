34612004dcbf60161faab8297c7a956f90df1626
mattsays <mattia.sicoli@gmail.com>
2025-01-20 19:40:56 +0000

    Changed "test_all" into "test"

--- build.zig
+++ build.zig
@@ -265,6 +265,6 @@ pub fn build(b: *std.Build) void {
 
     // Add a build step to run all unit tests.
     const run_unit_tests = b.addRunArtifact(unit_tests);
-    const test_step = b.step("test_all", "Run all unit tests");
+    const test_step = b.step("test", "Run all unit tests");
     test_step.dependOn(&run_unit_tests.step);
 }
