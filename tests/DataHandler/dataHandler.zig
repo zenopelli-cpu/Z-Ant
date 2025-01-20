d3a7618358609bdba3308b3827a97c4666bdf8d4
mattsays <mattia.sicoli@gmail.com>
2025-01-20 20:16:28 +0000

    Added heavy tests

--- tests/DataHandler/dataHandler.zig
+++ tests/DataHandler/dataHandler.zig
@@ -3,5 +3,5 @@ const std = @import("std");
 test {
     _ = @import("dataLoader.zig");
     _ = @import("dataProcessor.zig");
-    _ = @import("trainer.zig");
+    // _ = @import("trainer.zig"); // This test is added as a separate test in tests/Trainer/trainer.zig
 }
