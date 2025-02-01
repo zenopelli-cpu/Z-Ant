13d94c1a7822a4c0a4f81e0679544f273acae19c
MirkoCalvi <mirkocalvi.job@gmail.com>
2025-02-01 21:10:15 +0100

    draft codeGen

---
+++ src/codeGen/firstTry.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+const tensor = @import("tensor");
+const TensMath = @import("tensor_m");
+
+//Initializing wheight tensors
+
+const tensor = @import("tensor");
+const TensMath = @import("tensor_m");
+
+//predictive function
+pub fn predict() !void {
+    const tensor = @import("tensor");
+    const TensMath = @import("tensor_m");
+}
