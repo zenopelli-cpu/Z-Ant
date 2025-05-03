884f943b588d5440fd7b354255c20c5687e99908
MirkoCalvi <mirkocalvi.job@gmail.com>
2025-05-03 14:42:10 +0200

    Div op IR

---
+++ src/IR_graph/op_union/operators/op_div.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const allocator = std.heap.page_allocator;
+const zant = @import("../../../zant.zig");
+
+// --- onnx ---
+const onnx = zant.onnx;
+const ModelProto = onnx.ModelProto;
+const GraphProto = onnx.GraphProto;
+const NodeProto = onnx.NodeProto;
+const TensorProto = onnx.TensorProto;
+
+// --- zant ---
+const tensorZant = @import("../../tensorZant.zig");
+const TensorZant = tensorZant.TensorZant;
+
+// https://onnx.ai/onnx/operators/onnx__Div.html
+// INPUTS:
+//      - A (heterogeneous) - T: First operand.
+//      - B (heterogeneous) - T: Second operand.
+// OUTPUTS:
+//      - C (heterogeneous) - T: Result, has same element type as two inputs.
+pub const Div = struct {
+    input_A: *TensorZant,
+    input_B: *TensorZant,
+    output_C: *TensorZant,
+
+    pub fn init(nodeProto: *NodeProto) !Div {
+        const input_A = if (tensorZant.tensorMap.getPtr(nodeProto.input[0])) |ptr| ptr else return error.input_A_notFound;
+        const input_B = if (tensorZant.tensorMap.getPtr(nodeProto.input[1])) |ptr| ptr else return error.input_B_notFound;
+        const output_C = if (tensorZant.tensorMap.getPtr(nodeProto.output[0])) |ptr| ptr else return error.output_C_notFound;
+
+        return Div{
+            .input_A = input_A,
+            .input_B = input_B,
+            .output_C = output_C,
+        };
+    }
+
+    pub fn get_output_shape(self: Div) []usize {
+        const res: []usize = [_]usize{ 0, 0, 1, 1 };
+        res[0] += self.input;
+        return res;
+    }
+
+    pub fn print(self: Div) void {
+        std.debug.print("\n Div:\n {any}", .{self});
+    }
+};
