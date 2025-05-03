51f3c4f386d0152cccc603b1e5bb14443ea977df
MirkoCalvi <mirkocalvi.job@gmail.com>
2025-05-03 13:33:11 +0200

    Conv op IR

--- src/IR_graph/op_union/operators/op_conv.zig
+++ src/IR_graph/op_union/operators/op_conv.zig
@@ -13,32 +13,83 @@ const TensorProto = onnx.TensorProto;
 const tensorZant = @import("../../tensorZant.zig");
 const TensorZant = tensorZant.TensorZant;
 
+// https://onnx.ai/onnx/operators/onnx__Conv.html
+// INPUTS:
+//      - X (heterogeneous) - T: Input data tensor
+//      - W (heterogeneous) - T: The weight tensor
+//      - B (optional, heterogeneous) - T: Optional 1D bias to be added to the convolution, has size of M.
+// OUTPUTS:
+//      - Y (heterogeneous) - T: Output data tensor that contains the result of the convolution
+// ATTRIBUTES:
+//      - auto_pad - STRING (default is 'NOTSET'): auto_pad must be either NOTSET, SAME_UPPER, SAME_LOWER or VALID. Where default value is NOTSET
+//      - dilations - INTS : dilation value along each spatial axis of the filter. If not present, the dilation defaults is 1 along each spatial axis.
+//      - group - INT (default is '1'): number of groups input channels and output channels are divided into
+//      - kernel_shape - INTS : The shape of the convolution kernel. If not present, should be inferred from input W
+//      - pads - INTS : Padding for the beginning and ending along each spatial axis, it can take any value greater than or equal to 0.
+//      - strides - INTS : Stride along each spatial axis. If not present, the stride defaults is 1 along each spatial axis.
+
 pub const Conv = struct {
-    input: f32,
-    kernel: f16,
-    padding: []usize,
+    input_X: *TensorZant,
+    input_W: *TensorZant,
+    input_B: *TensorZant,
+    output_Y: *TensorZant,
+    //attributes:
+    auto_pad: []const u8,
+    dilations: ?[]i64,
+    group: i64,
+    kernel_shape: ?[]i64,
+    pads: ?[]i64,
+    strides: ?[]i64,
 
     pub fn init(nodeProto: *NodeProto) !Conv {
-        _ = nodeProto; //"details" will be a onnx struct
-        // Define the padding array
-        const padding_array = @constCast(&[_]usize{ 5, 6, 7, 8 });
+        const input_X = if (tensorZant.tensorMap.getPtr(nodeProto.input[0])) |ptr| ptr else return error.input_X_notFound;
+        const input_W = if (tensorZant.tensorMap.getPtr(nodeProto.input[1])) |ptr| ptr else return error.input_W_notFound;
+        const input_B = if (tensorZant.tensorMap.getPtr(nodeProto.input[2])) |ptr| ptr else return error.input_B_notFound;
+        const output_Y = if (tensorZant.tensorMap.getPtr(nodeProto.output[0])) |ptr| ptr else return error.output_Y_notFound;
 
-        // Create a slice from the array
-        const padding_slice: []usize = padding_array[0..];
+        var auto_pad: []const u8 = "NOTSET";
+        var dilations: ?[]i64 = null;
+        var group: i64 = 1;
+        var kernel_shape: ?[]i64 = null;
+        var pads: ?[]i64 = null;
+        var strides: ?[]i64 = null; //mandatory
+
+        for (nodeProto.attribute) |attr| {
+            if (std.mem.indexOf(u8, attr.name, "auto_pad")) |_| {
+                if (attr.type == onnx.AttributeType.STRING) auto_pad = attr.s else return error.ConvAuto_padNotSTRING;
+            } else if (std.mem.indexOf(u8, attr.name, "dilations")) |_| {
+                if (attr.type == onnx.AttributeType.INTS) dilations = attr.ints else return error.ConvDilatationNoINTS;
+            } else if (std.mem.indexOf(u8, attr.name, "group")) |_| {
+                if (attr.type == onnx.AttributeType.INT) group = attr.i else return error.ConvGroupNotINT;
+            } else if (std.mem.indexOf(u8, attr.name, "kernel_shape")) |_| {
+                if (attr.type == onnx.AttributeType.INTS) kernel_shape = attr.ints else return error.ConvKernelShapeNotINTS;
+            } else if (std.mem.indexOf(u8, attr.name, "pads")) |_| {
+                if (attr.type == onnx.AttributeType.INTS) pads = attr.ints else return error.ConvPadsNotINTS;
+            } else if (std.mem.indexOf(u8, attr.name, "strides")) |_| {
+                if (attr.type == onnx.AttributeType.INTS) strides = attr.ints else return error.ConvStridesNotINTS;
+            }
+        }
 
         return Conv{
-            .input = 10,
-            .kernel = 3.14,
-            .padding = padding_slice,
+            .input_X = input_X,
+            .input_W = input_W,
+            .input_B = input_B,
+            .output_Y = output_Y,
+            .auto_pad = auto_pad,
+            .dilations = dilations,
+            .group = group,
+            .kernel_shape = kernel_shape,
+            .pads = pads,
+            .strides = strides,
         };
     }
 
-    pub fn get_output_shape() []usize {
+    pub fn get_output_shape() []usize { //TODO
         const res: []usize = [_]usize{ 2, 2, 3, 3 };
         return res;
     }
 
-    pub fn print(self: Conv) void {
+    pub fn print(self: Conv) void { //TODO
         std.debug.print("\n CONV:\n {any}", .{self});
     }
 };
