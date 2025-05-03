41db134ec1c4f61cf76a132128e0c0aacefe81aa
MirkoCalvi <mirkocalvi.job@gmail.com>
2025-05-03 14:23:22 +0200

    Elu op IR

--- src/IR_graph/op_union/operators/operators.zig
+++ src/IR_graph/op_union/operators/operators.zig
@@ -1,6 +1,9 @@
 pub const Add = @import("op_add.zig").Add;
-pub const Sub = @import("op_sub.zig").Sub;
-pub const Conv = @import("op_conv.zig").Conv;
-pub const Useless = @import("op_useless.zig").Useless;
 pub const AveragePool = @import("op_averagePool.zig").AveragePool;
 pub const BatchNormalization = @import("op_batchNormalization.zig").BatchNormalization;
+pub const Conv = @import("op_conv.zig").Conv;
+pub const Elu = @import("op_elu.zig").Elu;
+
+pub const Sub = @import("op_sub.zig").Sub;
+
+pub const Useless = @import("op_useless.zig").Useless;
