eda5f7fb14551df3823519eab66d522d70c9c354
Marco DV <macsdeve@gmail.com>
2025-05-06 12:08:19 +0200

    fix import

--- src/zant.zig
+++ src/zant.zig
@@ -1,4 +1,4 @@
 pub const core = @import("Core/core.zig");
 pub const utils = @import("Utils/utils.zig");
 pub const onnx = @import("onnx/onnx.zig");
-pub const uops = @import("CodeGen/renderers/UOps.zig");
+pub const uops = @import("CodeGen/renderers/Uops.zig");
