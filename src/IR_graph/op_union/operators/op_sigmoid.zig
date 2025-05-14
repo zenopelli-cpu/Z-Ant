cb72688772037259e7274b7a49c7527a97c9ef8f
Manuel Fasanella <manuel.fasanella@mail.polimi.it>
2025-05-14 10:43:58 +0200

    sigmoid. write_op() / get_output_tensor() / get_output_shape()

--- src/IR_graph/op_union/operators/op_sigmoid.zig
+++ src/IR_graph/op_union/operators/op_sigmoid.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const allocator = std.heap.page_allocator;
 const zant = @import("../../../zant.zig");
+const tensorMath = zant.core.tensor.math_standard;
 
 // --- onnx ---
 const onnx = zant.onnx;
@@ -12,7 +13,9 @@ const TensorProto = onnx.TensorProto;
 // --- zant ---
 const tensorZant = @import("../../tensorZant.zig");
 const TensorZant = tensorZant.TensorZant;
-const tensorMath = zant.core.tensor.math_standard;
+const TensorCategory = tensorZant.TensorCategory;
+
+const utils = @import("../../../CodeGen/utils.zig");
 
 //https://onnx.ai/onnx/operators/onnx__Sigmoid.html
 // INPUTS:
@@ -34,7 +37,40 @@ pub const Sigmoid = struct {
     }
 
     pub fn get_output_shape(self: Sigmoid) []usize {
-        return self.output_Y.shape;
+        return self.output_Y.getShape();
+    }
+
+    pub fn get_output_tensor(self: Sigmoid) *TensorZant {
+        return self.output_Y;
+    }
+
+    pub fn write_op(self: Sigmoid, writer: std.fs.File.Writer) !void {
+        //----create tensor_X_string
+        var tensor_X_string: []u8 = undefined;
+        defer allocator.free(tensor_X_string);
+        if (self.input_X.tc == TensorCategory.INITIALIZER) {
+            tensor_X_string = try std.mem.concat(allocator, u8, &[_][]const u8{
+                "@constCast(&param_lib.tensor_",
+                try utils.getSanitizedName(self.input_X.name),
+                ")",
+            });
+        } else {
+            tensor_X_string = try std.mem.concat(allocator, u8, &[_][]const u8{ "&tensor_", try utils.getSanitizedName(self.input_X.name) });
+        }
+
+        _ = try writer.print(
+            \\
+            \\    tensMath.sigmoid_lean(
+            \\      T,
+            \\      {s},
+            \\      &tensor_{s},
+            \\    )
+        ,
+            .{
+                tensor_X_string,
+                try utils.getSanitizedName(self.output_Y.name),
+            },
+        );
     }
 
     pub fn compute_output_shape(self: Sigmoid) []usize {
