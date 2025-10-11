07583cf61197d6ca3790878de33887496a704fd9
MirkoCalvi <mirkocalvi.job@gmail.com>
2025-10-11 12:09:00 +0200

    deleted old build copy and fixed stm32n6 cmsis path

--- zantBuild/stm32n6_flags.zig
+++ zantBuild/stm32n6_flags.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub const Stm32n6_flags = struct {
     stm32n6_accel: bool,
-    stm32n6_cmsis_path: []const u8,
+    stm32n6_cmsis_path: ?[]const u8,
     stm32n6_force_native: bool,
     stm32n6_use_cmsis: bool,
     stm32n6_use_ethos: bool,
@@ -11,7 +11,7 @@ pub const Stm32n6_flags = struct {
     pub fn init(b: *std.Build) !Stm32n6_flags {
         return Stm32n6_flags{
             .stm32n6_accel = b.option(bool, "stm32n6_accel", "Enable STM32 N6 accelerator support") orelse false,
-            .stm32n6_cmsis_path = b.option([]const u8, "stm32n6_cmsis_path", "Optional CMSIS include path for STM32 N6 support") orelse "",
+            .stm32n6_cmsis_path = b.option([]const u8, "stm32n6_cmsis_path", "Optional CMSIS include path for STM32 N6 support"),
             .stm32n6_force_native = b.option(bool, "stm32n6_force_native", "Force STM32 N6 accelerator stubs on non-Thumb targets (useful for host testing)") orelse false,
             .stm32n6_use_cmsis = b.option(bool, "stm32n6_use_cmsis", "Enable CMSIS Helium kernels for STM32 N6") orelse false,
             .stm32n6_use_ethos = b.option(bool, "stm32n6_use_ethos", "Enable Ethos-U integration stubs for STM32 N6") orelse false,
